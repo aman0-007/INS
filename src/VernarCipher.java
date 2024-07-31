23fc0ebd3b86efc5f7d5002766bf8275549aced3
aman0-007 <dwivedi.aman2811@gmail.com>
2024-07-31 09:42:32 +0530

    Resolved merge conflict in src/Cipher.java

---
+++ src/VernarCipher.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class VernarCipher {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Cipher Text : ");
+        String plainText = sc.next();
+
+        System.out.print("\nEnter Key : ");
+        String cipherKey = sc.next();
+
+        System.out.println(checkLength(plainText,cipherKey));
+    }
+
+    public static String checkLength(String plainText, String key){
+        if(plainText.length()==key.length()){
+            return getCipher(plainText.toUpperCase(),key.toUpperCase());
+        }else{
+            return "Length of plain text and key do not match.";
+        }
+    }
+
+    public static String getCipher(String plainText, String key){
+        String cipherText = "";
+
+        int cipher[] = new int[key.length()];
+
+        for(int i = 0; i < key.length(); i++){
+            cipher[i] = plainText.charAt(i) + key.charAt(i) - 130;
+        }
+
+        for(int i = 0; i < key.length(); i++){
+            if(cipher[i] > 25){
+                cipher[i] = cipher[i] - 26;
+            }
+        }
+
+        for (int i = 0; i < key.length(); i++){
+            int c = cipher[i] + 65;
+
+            cipherText += (char) c;
+        }
+        return cipherText;
+    }
+}
